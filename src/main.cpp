eadeb2690d2419982528184e1ff724a8105c2801
ejagombar@gmail.com <ejagombar@gmail.com>
2022-06-23 00:58:37 +0100

    added searching for wifi funcitonality

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,45 @@
 */
 #include <M5Core2.h>
 #include <GDTouchKeyboard.h>
+#include "WiFi.h"
 
 /* After M5Core2 is started or reset
   the program in the setUp () function will be run, and this part will only be run once.
   在 M5Core2 启动或者复位后，即会开始执行setup()函数中的程序，该部分只会执行一次。 */
 void setup() {
-  M5.begin();  //Init M5Core.  初始化 M5Core2
-  M5.Lcd.setTextColor(YELLOW);  //Set the font color to yellow.  设置字体颜色为黄色
-  M5.Lcd.setTextSize(2);  //Set the font size.  设置字体大小为2
-  M5.Lcd.setCursor(65, 10); //Move the cursor position to (x, y).  移动光标位置到 (x, y)处
-  M5.Lcd.println("Button example"); //The screen prints the formatted string and wraps the line.  输出格式化字符串并换行
-  M5.Lcd.setCursor(3, 35);
-  M5.Lcd.println("Press button B for 700ms");
-  M5.Lcd.println("to clear screen.");
-  M5.Lcd.setTextColor(RED);
-  Serial.println("We finished the setup and welcome to the main program");
+  M5.begin(); 
+
+  // M5.Lcd.setTextColor(YELLOW);  //Set the font color to yellow.  设置字体颜色为黄色
+  // M5.Lcd.setTextSize(2);  //Set the font size.  设置字体大小为2
+  // M5.Lcd.setCursor(65, 10); //Move the cursor position to (x, y).  移动光标位置到 (x, y)处
+  // M5.Lcd.println("Button example"); //The screen prints the formatted string and wraps the line.  输出格式化字符串并换行
+  // M5.Lcd.setCursor(3, 35);
+  // M5.Lcd.println("Press button B for 700ms");
+  // M5.Lcd.println("to clear screen.");
+  // M5.Lcd.setTextColor(RED);
+  // Serial.println("We finished the setup and welcome to the main program");
+
+  M5.Lcd.setTextColor(WHITE);  
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.setCursor(65, 10);
+  M5.Lcd.println("Setup Wifi");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
 }
+
+void strengthToColour(int strength)
+{
+  uint16_t colour;
+  strength = abs(strength);
+  if (strength <= 75 ) {colour = GREEN;}
+  else if (strength <= 80) {colour = YELLOW;}
+  else {colour = RED;}
+  M5.Lcd.setTextColor(colour); 
+}
+
 
 /* After the program in setup() runs, it runs the program in loop()
 The loop() function is an infinite loop in which the program runs repeatedly
@@ -35,10 +58,40 @@ The loop() function is an infinite loop in which the program runs repeatedly
 loop()函数是一个死循环，其中的程序会不断的重复运行 */
 void loop() {
 
+  Serial.println("scan start");
 
-  String t = GDTK.run();
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+      M5.Lcd.println("no networks found");
+  } else {
+      M5.Lcd.print(n);
+      M5.Lcd.println(" networks found");
+      for (int i = 0; i < n; ++i) {
+          // Print SSID and RSSI for each network found
+          strengthToColour(WiFi.RSSI(i));
+          M5.Lcd.print(i + 1);
+          M5.Lcd.print(": ");
+          M5.Lcd.println(WiFi.SSID(i));
+
+          Serial.print(i + 1);
+          Serial.print(": ");
+          Serial.println(WiFi.SSID(i));
+          Serial.print(" (");
+          Serial.print(WiFi.RSSI(i));
+          Serial.print(")");
+          Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+          
+      }
+  }
+  delay(10000);
+  Serial.println("");
+
+  String t = GDTK.run("test:",0xefe1);
   Serial.println(t);
   delay(1000);
+
   
   // M5.update(); //Read the press state of the key.  读取按键 A, B, C 的状态
   // if (M5.BtnA.wasReleased() || M5.BtnA.pressedFor(1000, 200)) {
